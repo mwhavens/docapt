785d5decce3bffc499ae4315d2f3877538d5276e
Michael W. Havens <Michael.Havens@mihin.org>
2019-05-30 13:25:05 -0400

    Working with template

--- core/views.py
+++ core/views.py
@@ -3,4 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    #GET https://team02-rof.interopland.com/new-hope-services/fhir/Patient?name=Ben&_pretty=true
+    if request.method == 'POST':
+        print(f"request.POST = {request.POST}")
+        
+    return render(request, 'patient_search.html', {
+        'data': None
+    })
+    #return HttpResponse("Hello, world. You're at the polls index.")
