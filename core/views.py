6c05a6ae016a0e3bfee95a587ef59b2724da8d9a
Michael W. Havens <Michael.Havens@mihin.org>
2019-05-30 14:58:42 -0400

    Editted stuff

--- core/views.py
+++ core/views.py
@@ -28,16 +28,21 @@ def index(request):
     BASE_URL = 'https://team02-rof.interopland.com/new-hope-services/fhir/Patient'
     msg = ""
     if request.method == 'POST':
-        params = {'name': request.POST['fname'], '_pretty': 'true'}
+        params = {
+            'name': request.POST['name'],
+            'family': request.POST['email'],
+            'phone': request.POST['phone'],
+            '_pretty': 'true'
+        }
         print(f"request.POST = {request.POST}")
         response = requests.get(BASE_URL, params = params,
                                 auth=('mihin_hapi_fhir', 'cLQgfFT2oAgdzpXxA6jxRQxjZJSC5EurTwWx'))
         print(f"response = {response}")
         print(f"response.content = {response.text}")
-        #try:
-        msg = json.dumps(response.json(), indent=4, sort_keys=True)
-        #except:
-        #    msg = response.text
+        try:
+            msg = json.dumps(response.json(), indent=4, sort_keys=True)
+        except json.JSONDecodeError:
+            msg = response.text
     return render(request, 'patient_search.html', {
         'msg': msg
     })
